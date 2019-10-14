68d2d62f98df20e004c126078211805b756ad3be
z18ista03 <z18ista03@Z00P1-10.SPSKNM>
2019-10-14 09:12:11 +0200

    Add project files.

---
+++ Open-Lab-02.5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Open_Lab_02._5
+{
+    class Program
+    {
+        public static bool DivisibleByFive(int num)
+        {
+            return (num % 5 == 0);
+        }
+        static void Main(string[] args)
+        {
+            bool divisiblebyfive;
+            int num;
+
+            Console.WriteLine("Zadaj si číslo:");
+            num = int.Parse(Console.ReadLine());
+
+            divisiblebyfive = DivisibleByFive(num);
+            Console.WriteLine(divisiblebyfive);
+
+
+        }
+
+
+    }
+}
